ade17c6347a4bb607e37859849459b0bbe34e943
gayatrisathawane <gayatripsathawane@gmail.com>
2023-10-26 11:13:21 +0530

    signup api created

--- server/index.js
+++ server/index.js
@@ -14,43 +14,58 @@ const PORT = process.env.PORT || 5000
 
 
 //mongodb connection 
-const mongoDb = async() =>{
+const mongoDb = async () => {
 
     const connect = await mongoose.connect(process.env.MONGODB_URL)
 
-    if(connect)
-    {
+    if (connect) {
         console.log("mongodb connected")
     }
 
 }
 mongoDb()
 
+//sin up
+app.post('/signup', async (req, res) => {
 
-app.post('/signup' , async(req,res)=>{
+    const { name, email, mobile, address, password, gender } = req.body
 
-    const{name,email,mobile,address,password,gender}=req.body
-
-    const newUser =  new User({
-        name,email,mobile,address,password,gender
+    const newUser = new User({
+        name, email, mobile, address, password, gender
     })
 
-   const saveUser =  await newUser.save()
 
 
-   res.json({
-
-    data:saveUser,
-    success:true
-
-   })
-
+    try {
+        const saveUser = await newUser.save()
+        return res.json({
+            data: saveUser,
+            success: true,
+            message: "successfully created signup"
+        })
+    }
+    catch (e) {
+        return res.json(
+            {
+                message: (e.message)
+            }
+        )
+    }
 })
 
 
 
 
 
-app.listen(PORT ,()=>{
+
+//login user
+
+
+
+
+
+
+
+app.listen(PORT, () => {
     console.log(`port running on Port no  ${PORT}`)
 })
